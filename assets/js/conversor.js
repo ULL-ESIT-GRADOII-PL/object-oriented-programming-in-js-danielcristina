ee76842b61eb20d7c60d4b6a01d1cb831d991cb8
alu0100821338 <alu0100821338@ull.edu.es>
2016-03-01 19:05:53 +0000

    Añadiendo métodos toCelsius y toFarenheit

--- assets/js/conversor.js
+++ assets/js/conversor.js
@@ -1,8 +1,7 @@
 (function(exports) {
   "use strict";
 
-  function Medida(valor,tipo)
-  {
+  function Medida(valor,tipo) {
     this.type = tipo;
     this.value = valor;
     /* tipo es opcional. Debería admitir  new Medida("45.2 Km") */
@@ -10,8 +9,7 @@
 
   }
 
-  function Temperatura(valor,tipo)
-  {
+  function Temperatura(valor,tipo) {
     Medida.call(this, valor, tipo);
     /* tipo es opcional. Debería admitir new Medida("45.2 F") */
   }
@@ -19,22 +17,26 @@
   Temperatura.prototype = new Medida();
   Temperatura.prototype.constructor = Temperatura;
 
-  function Celsius(valor)
-  {
+  function Celsius(valor) {
     Temperatura.call(this, valor, "c");
+    //funcion Celsius to Farenheit
+    this.toFarenheit = function(){
+      return ((valor * 9/5)+32);
+    };
   }
   Celsius.prototype = new Temperatura();
   Celsius.prototype.constructor = Celsius;
 
-  function Farenheit(valor)
-  {
+  function Farenheit(valor) {
     Temperatura.call(this, valor, "f");
+    this.toCelsius = function(){
+      (valor - 32)*5/9;
+    };
   }
   Farenheit.prototype = new Temperatura();
   Farenheit.prototype.constructor = Farenheit;
 
-  function Kelvin(valor)
-  {
+  function Kelvin(valor) {
     Temperatura.call(this, valor, "k");
   }
   Kelvin.prototype = new Temperatura();
@@ -51,6 +53,11 @@
         elemento  = document.getElementById('converted'),
         /* Extienda la RegeExp a la especificación. use una XRegExp */
         regexp    = /^\s*([-+]?\d+(?:\.\d+)?(?:e[+-]?\d+)?)\s*([a-z,A-Z]+)\s*$/i,
+
+    /*    date = XRegExp('(?<year>  [0-9]{4} ) -?  # year  \n' +
+               '(?<month> [0-9]{2} ) -?  # month \n' +
+               '(?<day>   [0-9]{2} )     # day     ', 'x');
+    */
         valor     = valor.match(regexp);
 
     if (valor) {
@@ -63,13 +70,16 @@
       switch (tipo) {
         case 'c':
           var celsius = new Celsius(numero);
-          elemento.innerHTML = celsius.toFarenheit().toFixed(2) + " Farenheit";
+          elemento.innerHTML = celsius.toFarenheit().toFixed(2) + " Farenheit.";
           break;
         case 'f':
           var farenheit = new Farenheit(numero);
           elemento.innerHTML = farenheit.toCelsius().toFixed(2) + " Celsius";
           break;
-
+        /*case 'k':
+          var kelvin = new Kelvin(numero);
+          elemento.innerHTML = kelvin.toCelsius().toFixed(2) + " Celsius";
+          break;*/
         default:
           /* rellene este código */
       }
